6af400dd9cbf1fabb73192bdb370f1220af8fd49
LatinoAlejo <a.caicedom@uniandes.edu.co>
2019-04-12 16:31:44 -0500

    Tenía un error en info trayecto, ya no!!

--- src/app/info-trayecto/info-trayecto.module.ts
+++ src/app/info-trayecto/info-trayecto.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ListarInfoTrayectoComponent } from './listar-info-trayecto/listar-info-trayecto.component';
 import { CrearInfoTrayectoComponent } from './crear-info-trayecto/crear-info-trayecto.component';
 import { EditInfoTrayectoComponent } from './edit-info-trayecto/edit-info-trayecto.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [ListarInfoTrayectoComponent, CrearInfoTrayectoComponent, EditInfoTrayectoComponent], 
   exports: [ListarInfoTrayectoComponent, CrearInfoTrayectoComponent, EditInfoTrayectoComponent]
