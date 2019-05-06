83400168aacc7d3b2458bafcea28236074b521be
Daniel Peña <df.penap@uniandes.edu.co>
2019-05-06 12:56:29 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Busqueda } from '../busqueda';
+import { Ciudad } from '../ciudad';
+import { Usuario } from 'src/app/usuario/usuario';
+import { Vehiculo } from 'src/app/vehiculo/vehiculo';
+import { Peaje } from '../peaje';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +13,16 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  buscar = new Busqueda();
+  ciudades: Ciudad[];
+  usuarios: Usuario[];
+  peajes: Peaje[];
+  vehiculos: Vehiculo[];
 
   ngOnInit() {
+  }
+  submit(){
+  
   }
    openNav() {
     document.getElementById("mySidenav").style.width = "250px";
