4c2347a897434c7fabdb26dd55fb629990ca4ff0
Daniel Peña <df.penap@uniandes.edu.co>
2019-05-17 11:32:08 -0500

    Arreglos

--- src/app/notificacion/notificacion.service.ts
+++ src/app/notificacion/notificacion.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Notificacion } from './notificacion';
+const API_URL = "http://52a0903d.ngrok.io/s4_carpooling-api/api";
+const notificaciones = 'notificaciones';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class NotificacionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createNotificacion(notificacion)
+  {
+    return this.http.post<Notificacion>(API_URL + '/' + notificaciones, notificacion);
+  }
 }
