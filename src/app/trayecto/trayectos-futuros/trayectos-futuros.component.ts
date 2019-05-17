c9496d811d28a8fd0d65df9fc691dc0bf34bfa89
LatinoAlejo <a.caicedom@uniandes.edu.co>
2019-05-17 12:20:51 -0500

    Lógica de trayectos futuros funcionando

--- src/app/trayecto/trayectos-futuros/trayectos-futuros.component.ts
+++ src/app/trayecto/trayectos-futuros/trayectos-futuros.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../../usuario/usuario.service';
 import { UsuarioDetail } from '../../usuario/usuario-detail';
 import { Usuario } from '../../usuario/usuario';
+import { Trayecto } from '../trayecto';
+import { TrayectoService } from '../trayecto.service';
 
 @Component({
   selector: 'app-trayectos-futuros',
@@ -11,18 +13,29 @@ import { Usuario } from '../../usuario/usuario';
 export class TrayectosFuturosComponent implements OnInit {
 
   constructor(
-    private usuarioService: UsuarioService
+    private usuarioService: UsuarioService, private trayectoService: TrayectoService
   ) {}
 
    usuario : UsuarioDetail;
   usernameActual: string;
   usuarios:Usuario[];
+  trayectos: Trayecto[];
 
   ngOnInit() {
+    
     this.getCurretUsuario();
     this.usuarioService.getUsuarios().subscribe(user=>{this.usuarios=user});
 
+    console.log(this.usuario);
+    console.log(this.usuarios);
+    this.trayectos = this.usuario.trayectoActualConductor;
+    console.log(this.trayectos);
+
   }
+
+  getTrayectos(): void {
+    this.trayectoService.getTrayectos().subscribe(trayectos => this.trayectos = trayectos);
+}
 
 
   darUsuarioActual(): string {
