5b3e1830943c4fe48a0d83be143c76deddb74756
Daniel Peña <df.penap@uniandes.edu.co>
2019-05-22 17:05:31 -0500

    Arreglos visuales home

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -31,7 +31,17 @@ export class HomeComponent implements OnInit {
     private vehiculoservice: VehiculoService,
     config: NgbCarouselConfig
   ) {}
-
+ 
+  config: SwiperOptions = {
+    autoplay: 3000, // Autoplay option having value in milliseconds
+    initialSlide: 3, // Slide Index Starting from 0
+    slidesPerView: 3, // Slides Visible in Single View Default is 1
+    pagination: '.swiper-pagination', // Pagination Class defined
+    paginationClickable: true, // Making pagination dots clicable
+    nextButton: '.swiper-button-next', // Class for next button
+    prevButton: '.swiper-button-prev', // Class for prev button
+    spaceBetween: 30 // Space between each Item
+  };
   buscar = new Busqueda();
   retorno = new Array<Trayecto>();
   ciudades: Ciudad[];
